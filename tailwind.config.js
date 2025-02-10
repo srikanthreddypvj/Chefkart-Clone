a4c21d0fe0854a09ce2423889ea9cd8af5bee457
Panduga Venkata Jaya Srikanth Reddy <94885124+srikanthreddypvj@users.noreply.github.com>
2025-02-10 22:12:49 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'], // Add your Google Font here
+      },
+      letterSpacing: {
+        tightest: '-.075em',
+        tighter: '-.05em',
+        tight: '-.025em',
+        normal: '0',
+        wide: '.025em',
+        wider: '.05em',
+        widest: '.1em',
+        widest: '.25em',
+      },
+      width: {
+        '95rem': '95rem',
+      },
+
+
+    },
+  },
+
+  plugins: [],
+};
